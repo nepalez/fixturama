725fd9e6b99fa6719a3b351e3d5568115ac2c964
Andrew Kozin (nepalez) <andrew.kozin@gmail.com>
2021-04-03 21:23:30 +0300

    Support focused specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,9 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
